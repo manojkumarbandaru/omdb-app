29509b6b4991c1337caa53909775965a9021d554
Manoj Kumar Bandaru <manoj.bandaru@SLS.ads.valuelabs.net>
2020-01-01 16:53:39 +0530

    Updates

---
+++ precache-manifest.ac4dabf1e1b034524b1790a128311efd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd415f32a3965b71e2bc544c4f7504af",
+    "url": "/omdb-app/index.html"
+  },
+  {
+    "revision": "fed58939b30531e4ebd2",
+    "url": "/omdb-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "87cdd5a331ef7ec0e17e",
+    "url": "/omdb-app/static/js/2.3087554a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/omdb-app/static/js/2.3087554a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fed58939b30531e4ebd2",
+    "url": "/omdb-app/static/js/main.e50842aa.chunk.js"
+  },
+  {
+    "revision": "389c6b176552cf199b33",
+    "url": "/omdb-app/static/js/runtime-main.e8b3ee80.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/omdb-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
